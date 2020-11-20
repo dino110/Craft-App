7cc6735f5c14a2c3592230235726ff22e47bc703
Dino <dino.kotaranin@gmail.com>
2020-11-21 00:31:52 +0100

    mongoose added #3

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://dino110:Dinamo1986@craftappcluster.4qnks.mongodb.net/Craft-app",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("Connected to DB..");
+  })
+  .catch((err) => console.log(err.reason));
+
+//Client.create();
 
 // TODO
 // Add middleware (bodyparser)
 // Connect to mongoose
 // Import models module
 // Import controllers module
+
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello world");
