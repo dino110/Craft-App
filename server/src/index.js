c324a9df15567ca185d445b01a4b02ff231a3290
thinktwice13 <marioo.krajacic@gmail.com>
2020-11-20 10:08:48 +0100

    Add TODOs

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const app = express()
 
+//TODO
+// Add middleware (bodyparser)
+// Connect to mongo
+// Import models module
+// Import controllers module
+
+// Start on specific port
 app.listen(() => console.log('There will be dragons'))
